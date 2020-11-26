ce20efcaab4e357db354f126d1bd38b65a54dc72
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-11-26 01:02:38 -0500

    cambios para el hud

---
+++ resources/[Vehicles]/[AutosModificados]/truck/__resource.lua
@@ -0,0 +1,173 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+ 
+files {
+    'data/codestacker/vehicles.meta',
+    'data/codestacker/carvariations.meta',
+    'data/codestacker/carcols.meta',
+    'data/codestacker/handling.meta',
+    'data/codestacker/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/codestacker/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/codestacker/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/codestacker/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/codestacker/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/codestacker/vehiclelayouts.meta'
+ 
+files {
+    'data/GODz20HDRIG/vehicles.meta',
+    'data/GODz20HDRIG/carvariations.meta',
+    'data/GODz20HDRIG/carcols.meta',
+    'data/GODz20HDRIG/handling.meta',
+    'data/GODz20HDRIG/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/GODz20HDRIG/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/GODz20HDRIG/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/GODz20HDRIG/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/GODz20HDRIG/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/GODz20HDRIG/vehiclelayouts.meta'
+ 
+files {
+    'data/GODzEnclosed/vehicles.meta',
+    'data/GODzEnclosed/carvariations.meta',
+    'data/GODzEnclosed/carcols.meta',
+    'data/GODzEnclosed/handling.meta',
+    'data/GODzEnclosed/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/GODzEnclosed/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/GODzEnclosed/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/GODzEnclosed/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/GODzEnclosed/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/GODzEnclosed/vehiclelayouts.meta'
+ 
+files {
+    'data/Goliath/vehicles.meta',
+    'data/Goliath/carvariations.meta',
+    'data/Goliath/carcols.meta',
+    'data/Goliath/handling.meta',
+    'data/Goliath/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/Goliath/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/Goliath/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/Goliath/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/Goliath/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/Goliath/vehiclelayouts.meta' 
+
+files {
+    'data/street350/vehicles.meta',
+    'data/street350/carvariations.meta',
+    'data/street350/carcols.meta',
+    'data/street350/handling.meta',
+    'data/street350/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/street350/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/street350/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/street350/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/street350/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/street350/vehiclelayouts.meta' 
+
+files {
+    'data/StreetBeast/vehicles.meta',
+    'data/StreetBeast/carvariations.meta',
+    'data/StreetBeast/carcols.meta',
+    'data/StreetBeast/handling.meta',
+    'data/StreetBeast/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/StreetBeast/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/StreetBeast/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/StreetBeast/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/StreetBeast/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/StreetBeast/vehiclelayouts.meta'
+ 
+files {
+    'data/snailbike/vehicles.meta',
+    'data/snailbike/carvariations.meta',
+    'data/snailbike/carcols.meta',
+    'data/snailbike/handling.meta',
+    'data/snailbike/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/snailbike/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/snailbike/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/snailbike/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/snailbike/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/snailbike/vehiclelayouts.meta'
+
+files {
+    'data/snailklr/vehicles.meta',
+    'data/snailklr/carvariations.meta',
+    'data/snailklr/carcols.meta',
+    'data/snailklr/handling.meta',
+    'data/snailklr/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/snailklr/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/snailklr/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/snailklr/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/snailklr/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/snailklr/vehiclelayouts.meta'
+
+files {
+    'data/MadMax/vehicles.meta',
+    'data/MadMax/carvariations.meta',
+    'data/MadMax/carcols.meta',
+    'data/MadMax/handling.meta',
+    'data/MadMax/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/MadMax/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/MadMax/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/MadMax/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/MadMax/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/MadMax/vehiclelayouts.meta'
+
+files {
+    'data/AllStarGolf/vehicles.meta',
+    'data/AllStarGolf/carvariations.meta',
+    'data/AllStarGolf/carcols.meta',
+    'data/AllStarGolf/handling.meta',
+    'data/AllStarGolf/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/AllStarGolf/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/AllStarGolf/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/AllStarGolf/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/AllStarGolf/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/AllStarGolf/vehiclelayouts.meta'
+
+
+files {
+    'data/AllStarRv/vehicles.meta',
+    'data/AllStarRv/carvariations.meta',
+    'data/AllStarRv/carcols.meta',
+    'data/AllStarRv/handling.meta',
+    'data/AllStarRv/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/AllStarRv/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/AllStarRv/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/AllStarRv/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/AllStarRv/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/AllStarRv/vehiclelayouts.meta'
+
+files {
+    'data/brownsugar/vehicles.meta',
+    'data/brownsugar/carvariations.meta',
+    'data/brownsugar/carcols.meta',
+    'data/brownsugar/handling.meta',
+    'data/brownsugar/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/brownsugar/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/brownsugar/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/brownsugar/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/brownsugar/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/brownsugar/vehiclelayouts.meta'
+
+
