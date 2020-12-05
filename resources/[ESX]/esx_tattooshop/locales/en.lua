a097c425621ff3a33b06e4b43edf0d1cfacd70f3
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-12-05 12:32:57 -0500

    cambios para agregar barber y tatto

---
+++ resources/[ESX]/esx_tattooshop/locales/en.lua
@@ -0,0 +1,11 @@
+Locales ['en'] = {
+	['tattoo_shop_nearby'] = 'presione ~INPUT_PICKUP~ para acceder a la ~y~tienda de tatoo~s~.',
+	['money_amount']       = '<span style = "color: green;"> $%s</span>',
+	['part']               = 'parte',
+	['go_back_to_menu']    = '< Rgresar',
+	['tattoo']             = 'tattoo',
+	['tattoos']            = 'tattoos',
+	['tattoo_shop']        = 'tienda de tatoo',
+	['bought_tattoo']      = 'comprastes un ~y~tattoo~s~ por ~r~$%s~s~',
+	['not_enough_money']   = 'no tienes ~r~suficiente dinero~s~ para este tattoo! Lo perdistes ~r~$%s~s~'
+}
