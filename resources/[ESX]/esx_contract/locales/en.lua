17b203123289e1f668b7941c228f22fd2927b674
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-12-07 07:58:29 -0500

    cambios para contarto

---
+++ resources/[ESX]/esx_contract/locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+  ['writingcontract'] = 'Contrato para la siguiente matrícula: %s',
+  ['soldvehicle'] = 'Vendiste el vehículo con el número de registro ~r~%s~s~',
+  ['boughtvehicle'] = 'Compró el vehículo con el número de registro ~g~%s~s~',
+  ['notyourcar'] = 'Esto no es un vehículo',
+  ['nonearby'] = 'No hay vehículo cerca',
+  ['nonearbybuyer'] = 'NO hay comprador cerca',
+}
