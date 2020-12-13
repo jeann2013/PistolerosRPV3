a00c69d9e1ff4c2edf109f55cffd94d0714fbe96
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-12-12 21:41:56 -0500

    se agrego radar para los excesos de velocidad

---
+++ resources/[ESX]/esx_radars/config.lua
@@ -0,0 +1,5 @@
+Config 			= {}
+Config.Fine 	= 600 -- 10mph over (default: $300)
+Config.Fine2 	= 700 -- 20mph over (default: $400)
+Config.Fine3	= 800 -- 30mph over (default: $500)
+Config.Fine4 	= 900 -- 40mph over (default: $600)
