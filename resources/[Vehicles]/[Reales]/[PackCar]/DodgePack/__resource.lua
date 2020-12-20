e381194522582b85673cdca3126a4b8a876be52a
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-12-19 22:12:59 -0500

    nuevos autos

---
+++ resources/[Vehicles]/[Reales]/[PackCar]/DodgePack/__resource.lua
@@ -0,0 +1,86 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+ --FlatbedDually3500
+files {
+    'data/FlatbedDually3500/vehicles.meta',
+    'data/FlatbedDually3500/carvariations.meta',
+    'data/FlatbedDually3500/carcols.meta',
+    'data/FlatbedDually3500/handling.meta',
+    'data/FlatbedDually3500/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/FlatbedDually3500/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/FlatbedDually3500/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/FlatbedDually3500/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/FlatbedDually3500/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/FlatbedDually3500/vehiclelayouts.meta'
+
+ --RamDually3500
+files {
+    'data/RamDually3500/vehicles.meta',
+    'data/RamDually3500/carvariations.meta',
+    'data/RamDually3500/carcols.meta',
+    'data/RamDually3500/handling.meta',
+    'data/RamDually3500/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/RamDually3500/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/RamDually3500/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/RamDually3500/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/RamDually3500/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/RamDually3500/vehiclelayouts.meta'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
