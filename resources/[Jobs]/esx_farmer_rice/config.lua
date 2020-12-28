0c1c8c37678994b3d0652be2564e0193329aa1bc
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-12-27 22:46:12 -0500

    cambios para la descripcion de los iconos

--- resources/[Jobs]/esx_farmer_rice/config.lua
+++ resources/[Jobs]/esx_farmer_rice/config.lua
@@ -8,8 +8,8 @@ config.limit = {
 	rice_pro = 100
 }
 config.zones = {
-	farm = { coords = vector3(261.40, 6461.20, 31.79), name = 'Granja', color = 33, sprite = 514 },
+	farm = { coords = vector3(261.40, 6461.20, 31.79), name = 'Zona de arroz', color = 33, sprite = 514 },
 	process = { coords = vector3(2309.8, 4886.20, 41.89), name = 'Procesamiento de arroz', color = 33, sprite = 478 },
-	cow = { coords = vector3(430.0, 6467.0, 28.77), name = 'Vacas', color = 33, sprite = 473 },
-	dealer = { coords = vector3(108.30, -1799.50, 27.09), name = 'Comprador de Materia prima', color = 33, sprite = 108 },
+	cow = { coords = vector3(430.0, 6467.0, 28.77), name = 'Tractor para recolectar arroz', color = 33, sprite = 473 },
+	dealer = { coords = vector3(108.30, -1799.50, 27.09), name = 'Comprador de arroz', color = 33, sprite = 108 },
 }
