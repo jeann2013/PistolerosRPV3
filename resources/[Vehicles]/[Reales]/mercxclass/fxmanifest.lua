b68fe8f3322fe1cb0c71b6d9a9de10a9c7fd2db3
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2021-01-03 17:39:29 -0500

    se agrego mercedes

---
+++ resources/[Vehicles]/[Reales]/mercxclass/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious' 
+game 'gta5' 
+
+description 'Mercedes X-Class Pickup - (2018)'
+
+files {
+    'data/**/*.meta',
+    'data/**/*.ytyp'
+}
+
+
+data_file 'HANDLING_FILE'			'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'	'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE'			'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/**/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
+
+client_script 'vehicle_names.lua'
