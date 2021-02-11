1d29e33b3cb8c2dd5ac0d602f5ff90ae3da37e80
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2021-02-10 21:25:42 -0500

    se agrego para trapichar en la basura

---
+++ resources/[ESX]/esx_dumpsters/config.lua
@@ -0,0 +1,35 @@
+Config = {
+	EnableWeapons = true, -- If you want the players to be able to find weapons (false by default).
+	SearchTime = 10000, -- How much time it takes to search in millisecond.
+
+	Dumpsters = { -- Props of the dumpsters wich is getting defined in the client.
+		"prop_dumpster_01a",
+		"prop_dumpster_02a",
+		"prop_dumpster_02b",
+		"prop_dumpster_3a",
+        "prop_dumpster_3step",
+        "prop_dumpster_4a",
+        "prop_dumpster_4b",
+	},
+
+	Items = {
+        "bread",
+        "water",
+		"condomu",
+		"condom",
+		"lighter",
+		"cigarett"
+	},
+	
+	Weapons = {         
+        "WEAPON_BOTTLE"
+    }
+}
+
+Strings = { -- Translation
+	["Search"] = "Presione [~g~H~s~] para buscar",
+	["Searched"] = "¡Ya has buscado en este contenedor!",
+	["Found"] = "Coseguistes algo ",
+	["Searching"] = "Buscando...",
+	["Nothing"] = "No conseguistes nada..."
+}
