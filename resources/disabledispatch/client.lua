ceb63e9ba3f715221f730a51f59603856d071d14
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2021-07-12 23:42:29 -0500

    se cambio la agresividad de los NPC's

--- resources/disabledispatch/client.lua
+++ resources/disabledispatch/client.lua
@@ -4,7 +4,7 @@ Citizen.CreateThread(function()
         for i = 1, 12 do
             EnableDispatchService(i,false)
         end
-        --SetEveryoneIgnorePlayer(PlayerId(), 1)
+        SetEveryoneIgnorePlayer(PlayerId(), 1)
         SetPlayerWantedLevel(PlayerId(), 0, false)
 		SetPlayerWantedLevelNow(PlayerId(), false)
 		SetPlayerWantedLevelNoDrop(PlayerId(), 0, false)
