b86f951e61641a6b14dda92358c1c7ccb68803e7
Jean Carlos Nuñez <jeancarlosn2008@gmail.com>
2021-07-31 19:53:43 -0500

    nuevo menu

--- resources/[ui]/esx_menu_default_old/client/main.lua
+++ resources/[ui]/esx_menu_default_old/client/main.lua
@@ -1,5 +1,4 @@
 ESX = nil
-local soundOn = true
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -72,15 +71,10 @@ Citizen.CreateThread(function()
 
 			if data.elements[i].selected then
 				menu.setElement(i, 'selected', true)
-				if soundOn == true then
-					PlaySound(0, "Apt_Style_Purchase", "DLC_APT_Apartment_SoundSet", 0, 0, 1);
-				end
 			else
 				menu.setElement(i, 'selected', false)
-				if soundOn == true then
-					PlaySound(0, "Apt_Style_Purchase", "DLC_APT_Apartment_SoundSet", 0, 0, 1);	
-				end		
 			end
+
 		end
 
 		if menu.change ~= nil then
