781d97e7af9de4e82f5715832ca17bef06645661
Jean Carlos Nuñez <jeancarlosn2008@gmail.com>
2021-08-06 18:43:29 -0500

    donw time drugsales

--- resources/[ESX]/esx_npcdrugsales/client/main.lua
+++ resources/[ESX]/esx_npcdrugsales/client/main.lua
@@ -27,53 +27,65 @@ end)
 currentped = nil
 Citizen.CreateThread(function()
 
+local counter = 0;
+
 while true do
   Wait(1)
   local player = GetPlayerPed(-1)
   local playerloc = GetEntityCoords(player, 0)
   local handle, ped = FindFirstPed()
-  repeat
+  repeat    
     success, ped = FindNextPed(handle)
     local pos = GetEntityCoords(ped)
     local distance = GetDistanceBetweenCoords(pos.x, pos.y, pos.z, playerloc['x'], playerloc['y'], playerloc['z'], true)
+    if distance <= 1 and ped  ~= GetPlayerPed(-1) and ped ~= oldped  then
+      if IsPedInAnyVehicle(GetPlayerPed(-1)) == false then
+        if DoesEntityExist(ped)then
+          if IsPedDeadOrDying(ped) == false then
+            if IsPedInAnyVehicle(ped) == false then
+              local pedType = GetPedType(ped)
+              if pedType ~= 28 and IsPedAPlayer(ped) == false then
+                currentped = pos
 
-    if IsPedInAnyVehicle(GetPlayerPed(-1)) == false then
-      if DoesEntityExist(ped)then
-        if IsPedDeadOrDying(ped) == false then
-          if IsPedInAnyVehicle(ped) == false then
-            local pedType = GetPedType(ped)
-            if pedType ~= 28 and IsPedAPlayer(ped) == false then
-              currentped = pos
-
-              if distance <= 1 and ped  ~= GetPlayerPed(-1) and ped ~= oldped then
-                TriggerServerEvent('checkD')
-                if has == true then
-                    drawTxt(0.90, 1.40, 1.0,1.0,0.4, "Presione ~g~E ~w~para intentar un negocio de drogas...", 255, 255, 255, 255)
-                    if IsControlJustPressed(1, 86) then
-                      oldped = ped
-                      SetEntityAsMissionEntity(ped)
-                      TaskStandStill(ped, 9.0)
-                      pos1 = GetEntityCoords(ped)
-                      TriggerServerEvent('drugs:trigger')
-                      Citizen.Wait(2850)
-                      TriggerEvent('sell')
-                      SetPedAsNoLongerNeeded(oldped)
-                    end
-                else
-                  TriggerServerEvent('checkFoodDrugs')
-                  if hasFood == true then
-                    drawTxt(0.90, 1.40, 1.0,1.0,0.4, "Presione ~g~E ~w~para intentar una venta de comida...", 255, 255, 255, 255)
-                    if IsControlJustPressed(1, 86) then
+                
+                  if counter == 0 then
+                    TriggerServerEvent('checkD')
+                  end
+                  if has == true then
+                      drawTxt(0.90, 1.40, 1.0,1.0,0.4, "Presione ~g~E ~w~para intentar un negocio de drogas...", 255, 255, 255, 255)
+                      if IsControlJustPressed(1, 86) then
                         oldped = ped
                         SetEntityAsMissionEntity(ped)
                         TaskStandStill(ped, 9.0)
                         pos1 = GetEntityCoords(ped)
-                        TriggerServerEvent('foods:trigger')
+                        TriggerServerEvent('drugs:trigger')
                         Citizen.Wait(2850)
-                        TriggerEvent('sellfood')
+                        TriggerEvent('sell')
                         SetPedAsNoLongerNeeded(oldped)
+                      end
+                  else
+                    if counter == 0 then
+                      TriggerServerEvent('checkFoodDrugs')
                     end
-                  end
+                    if hasFood == true then
+                      drawTxt(0.90, 1.40, 1.0,1.0,0.4, "Presione ~g~E ~w~para intentar una venta de comida...", 255, 255, 255, 255)
+                      if IsControlJustPressed(1, 86) then
+                          oldped = ped
+                          SetEntityAsMissionEntity(ped)
+                          TaskStandStill(ped, 9.0)
+                          pos1 = GetEntityCoords(ped)
+                          TriggerServerEvent('foods:trigger')
+                          Citizen.Wait(2850)
+                          TriggerEvent('sellfood')
+                          SetPedAsNoLongerNeeded(oldped)
+                      end
+                    end
+                  end                
+                            
+                counter=counter+1
+              
+                if counter >= 12 then
+                  counter = 0
                 end
               end
             end
