f24d56d5b4cb208f1e018de0ecf70fa0b742e318
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2021-11-18 19:13:07 -0500

    new wash money in the city

--- resources/[ESX]/esx_moneywash/config.lua
+++ resources/[ESX]/esx_moneywash/config.lua
@@ -6,11 +6,12 @@ Config.MarkerColor  = {r = 100, g = 100, b = 204}
 Config.ZDiff        = 2.0
 Config.BlipSprite   = 431
 Config.Locale = 'en'
-Config.Percentage = 0.50 --Porcentaje de Comision 0.02 = 2%
+Config.Percentage = 0.30 --Porcentaje de Comision 0.02 = 2%
 
 Config.WASH = {
     { ['x'] =  1593.98,  ['y'] = 3594.15,  ['z'] = 38.77},
     { ['x'] =  2175.16,  ['y'] = 3322.19,  ['z'] = 46.47},
+    { ['x'] =  1136.34,  ['y'] = -991.92,  ['z'] = 46.11},
 }
 Config.Map = {
   {name="Dinero Sucio",  		color=6, scale=0.8, id=500, x = 1273.2139892578, y = -1711.6552734375, z = 54.77144241333}
