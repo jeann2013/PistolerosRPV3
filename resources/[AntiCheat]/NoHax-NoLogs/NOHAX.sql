18c21607b9e98743c1f054ec0588bb431ef168ed
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2021-12-07 00:56:19 -0500

    add anticheat

---
+++ resources/[AntiCheat]/NoHax-NoLogs/NOHAX.sql
@@ -0,0 +1,41 @@
+﻿-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.32-log - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.1.0.6116
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para server
+CREATE DATABASE IF NOT EXISTS `server` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `server`;
+
+-- Volcando estructura para tabla server.baneos_nohax
+CREATE TABLE IF NOT EXISTS `baneos_nohax` (
+  `license` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `identifier` varchar(25) COLLATE utf8mb4_bin DEFAULT NULL,
+  `liveid` varchar(21) COLLATE utf8mb4_bin DEFAULT NULL,
+  `xblid` varchar(21) COLLATE utf8mb4_bin DEFAULT NULL,
+  `discord` varchar(30) COLLATE utf8mb4_bin DEFAULT NULL,
+  `playerip` varchar(25) COLLATE utf8mb4_bin DEFAULT NULL,
+  `targetplayername` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `sourceplayername` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `reason` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `timeat` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `expiration` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `permanent` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ resources/[AntiCheat]/NoHax-NoLogs/configs/config_sv.lua
@@ -0,0 +1,14 @@
+ConfigServer = {}
+
+ConfigServer.LogModelos = "DISCORD WEEBHOOK"
+ConfigServer.LogExplosiones = "DISCORD WEEBHOOK"
+
+ConfigServer.BaneosWebHook = "DISCORD WEEBHOOK" 
+ConfigServer.LogAntiVPN = "DISCORD WEEBHOOK" 
+
+ConfigServer.NombreDelServidor = "NOMBRE DEL SERVIDOR" 
+ConfigServer.txadmin = false
+
+ConfigServer.Version = 1.2
+
+ConfigServer.Licencia = "DOMADO POR TEVI UwU"
